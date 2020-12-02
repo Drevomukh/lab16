b042837a60a9b32ba85f3199d95b9702dd7c5029
Drevomukh <70793125+Drevomukh@users.noreply.github.com>
2020-12-02 12:40:12 +0300

    first commit

---
+++ src/lab16/Item.java
@@ -0,0 +1,7 @@
+package lab16;
+
+public interface Item {
+    public double getPrice();
+    public String getName();
+    public String getDescription();
+}
---
+++ src/lab16/RestaurantOrder.java
@@ -0,0 +1,6 @@
+package lab16;
+
+public class RestaurantOrder extends OrderManager {
+    public RestaurantOrder() {}
+
+}
